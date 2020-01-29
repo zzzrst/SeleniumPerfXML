c7356aca5e1730a6b7496517e02fdc6af5f5f6c1
chronoreaper <chris.dd.29@gmail.com>
2020-01-29 14:52:01 -0500

    Update CloseBrowser.cs

--- SeleniumPerfXML/Implementations/ConcreteTestSteps/CloseBrowser.cs
+++ SeleniumPerfXML/Implementations/ConcreteTestSteps/CloseBrowser.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="OpenBrowser.cs" company="PlaceholderCompany">
+﻿// <copyright file="CloseBrowser.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
