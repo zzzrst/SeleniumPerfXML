fb34bf233abb462d95e70fd327c188bc12ad2757
chronoreaper <chris.dd.29@gmail.com>
2020-01-29 15:05:52 -0500

    Added the remaining two status class

---
+++ SeleniumPerfXML/Implementations/Statuses/TestCaseStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutomationTestSetFramework;
+
+namespace SeleniumPerfXML.Implementations
+{
+    class TestCaseStatus : ITestCaseStatus
+    {
+    }
+}
---
+++ SeleniumPerfXML/Implementations/Statuses/TestSetStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutomationTestSetFramework;
+
+namespace SeleniumPerfXML.Implementations
+{
+    class TestSetStatus : ITestSetStatus
+    {
+    }
+}
