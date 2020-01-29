4635fadf6fc475ccba48f03a760200879ebf0a02
chronoreaper <chris.dd.29@gmail.com>
2020-01-29 14:49:05 -0500

    Create TestCaseXml.cs

---
+++ SeleniumPerfXML/Implementations/TestCaseXml.cs
@@ -0,0 +1,73 @@
+﻿namespace SeleniumPerfXML.Implementations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using AutomationTestSetFramework;
+
+    /// <summary>
+    /// Implementation of the testCase class.
+    /// </summary>
+    public class TestCaseXml : ITestCase
+    {
+        /// <inheritdoc/>
+        public string Name { get; set; }
+
+        /// <inheritdoc/>
+        public int TestCaseNumber { get; set; }
+
+        /// <inheritdoc/>
+        public int TotalTestSteps { get; set; }
+
+        /// <inheritdoc/>
+        public ITestCaseStatus TestCaseStatus { get; }
+
+        /// <inheritdoc/>
+        public int CurrTestStepNumber { get; set; }
+
+        /// <inheritdoc/>
+        public IMethodBoundaryAspect.FlowBehavior OnExceptionFlowBehavior { get; set; }
+
+        /// <inheritdoc/>
+        public bool ExistNextTestStep()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ITestStep GetNextTestStep()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void HandleException(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void SetUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public bool ShouldExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void UpdateTestCaseStatus(ITestStepStatus testStepStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
