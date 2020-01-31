7c924c5b20aa97e7af57b1fbc7861174eb31576e
chronoreaper <chris.dd.29@gmail.com>
2020-01-31 14:55:35 -0500

    Update ReflectiveGetter.cs

--- SeleniumPerfXML/Implementations/ReflectiveGetter.cs
+++ SeleniumPerfXML/Implementations/ReflectiveGetter.cs
@@ -2,7 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace SeleniumPerfXML.TestActions
+namespace SeleniumPerfXML.Implementations
 {
     using System;
     using System.Collections.Generic;
