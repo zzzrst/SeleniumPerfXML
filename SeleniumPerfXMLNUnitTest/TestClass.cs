6d10bc9eb1e4a9d0d6130fc77bd8f426ccf4fb69
chronoreaper <chris.dd.29@gmail.com>
2020-02-04 10:47:57 -0500

    Added test case class

---
+++ SeleniumPerfXMLNUnitTest/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using SeleniumPerfXML;
+
+namespace SeleniumPerfXMLNUnitTest
+{
+    class TestClass
+    {
+        [Test]
+        public void TestBareMinimum()
+        {
+
+        }
+    }
+}
