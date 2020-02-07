fdafa7c7534ce02dba5bbfb9a03339bff6e6d626
chronoreaper <chris.dd.29@gmail.com>
2020-02-07 08:51:07 -0500

    Added fake test steps

---
+++ SeleniumPerfXML/Implementations/ConcreteTestSteps/FakeFailingTestStep.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="FakeFailingTestStep.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SeleniumPerfXMLNUnitTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using SeleniumPerfXML.Implementations;
+
+    /// <summary>
+    /// This test step will always fail.
+    /// </summary>
+    public class FakeFailingTestStep : TestStepXml
+    {
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "FakeFailingTestStep";
+
+        /// <inheritdoc/>
+        public override void Execute()
+        {
+            base.Execute();
+            throw new Exception("Fake Test Step Failed!");
+        }
+    }
+}
---
+++ SeleniumPerfXML/Implementations/ConcreteTestSteps/FakeTestStep.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="FakeTestStep.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SeleniumPerfXMLNUnitTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using SeleniumPerfXML.Implementations;
+
+    /// <summary>
+    /// This test step is the same as testStepXml.
+    /// </summary>
+    public class FakeTestStep : TestStepXml
+    {
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "FakeTestStep";
+    }
+}
